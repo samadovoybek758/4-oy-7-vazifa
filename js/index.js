efbe28091100fe2436d365f0600e2770f096274e
samadovoybek758 <oybek22samadov@gmail.com>
2024-06-13 22:03:44 +0500

    firstly

---
+++ js/index.js
@@ -0,0 +1,312 @@
+// 1-masala
+// function colors(color1,color2,color3) {
+//     let res =[color1,color2,color3];
+//     return res
+// }
+// console.log(colors("red",'blue','green'));
+
+
+
+// 2-masala  
+// function numbers(num1,num2,num3) {
+//     let res =[num1,num2,num3];
+//     return res
+// }
+// console.log(numbers(2,45,32));
+
+
+// 3-masala    !!!
+// function colors(color1,color2,color3) {
+//     let res =[color1,color2,color3];
+//     return res[0],res[res.length-1];
+    
+// }
+// console.log(colors("red",'blue','green'));
+
+
+
+// 4-masala
+// function colors(color1,color2,color3) {
+//     let res =[color1,color2,color3];
+//     res[1]="brown"
+//     return res;
+// }
+// console.log(colors("red",'blue','green'));
+
+
+// 5-masala
+// function colors(color1,color2,color3) {
+//     let res =[color1,color2,color3];
+//     res.unshift ('black')
+//     return res
+// }
+// console.log(colors("red",'blue','green'));
+
+
+// 6-masala
+// function colors(color1,color2,color3) {
+//     let res =[color1,color2,color3];
+//     res.pop()
+//     return res
+// }
+// console.log(colors("red",'blue','green'));
+
+
+// 7-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     return res.length
+// }
+// console.log(arr("1",'blue',true));
+
+
+
+// 8-masala
+// function colors(color1,color2,color3) {
+//     let res =[color1,color2,color3];
+//     res.unshift('6')
+//     return res
+// }
+// console.log(colors("red",'blue','green'));
+
+
+
+// 9-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.shift()
+//     return res
+// }
+// console.log(arr("1",'blue',true));
+
+
+
+
+// 10-masala
+
+// 11-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//      return res.includes("blue")
+// }
+// console.log(arr("1",'blue',true));
+
+
+
+// 12-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//      return res.sort()
+// };
+// console.log(arr('blue',"apple","city"));
+
+
+// 13-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     return res.indexOf('blue')
+// };
+// console.log(arr("1",'blue',true));
+
+
+
+// 14-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.splice(1,1);
+//     return res
+// }
+// console.log(arr("hello",'blue',true));
+
+
+
+// 15-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.pop();
+//     res.pop();
+//     return res
+// }
+//  console.log(arr("hello",'blue',true));
+
+
+
+// 16-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.splice(2,0,"salom","hayr")
+//     return res
+// }
+//  console.log(arr("hello",'blue',true));
+
+
+// 17-masala !!!
+
+
+// 18-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.splice(0,res.length)
+//     return res
+// }
+//  console.log(arr("hello",'blue',true));
+
+
+// 19-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     return res
+// }
+//  console.log(arr("hello",'blue',true));
+
+
+
+
+
+// massiv metodlariga oid masalalar
+// 1-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.push("salom")
+//     return res
+// }
+//  console.log(arr("hello",'blue',true));
+
+
+// 2-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.pop()
+//     return res
+// }
+//  console.log(arr("hello",'blue',true));
+
+
+
+// 3-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.shift();
+//     return res
+// }
+//  console.log(arr("hello",'blue',true));
+
+
+
+// 4-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.unshift("world");
+//     return res
+// }
+//  console.log(arr("hello",'blue',true));
+
+
+// 5-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//      return res.includes('blue');
+// }
+// console.log(arr("hello",'blue',true));
+
+
+// 6-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//      return res.indexOf('blue');
+// }
+// console.log(arr("hello",'blue',true));
+
+
+// 7-masala
+// function arr(arg1,arg2,arg3,arg4) {
+//     let res =[arg1,arg2,arg3,arg4];
+//      return res.lastIndexOf('blue');
+// }
+// console.log(arr("hello",'blue',true,"blue"));
+
+
+// 8-masala
+// function arr(arg1,arg2,arg3,arg4) {
+//     let res =[arg1,arg2,arg3,arg4];
+//     res.splice(1,2);
+//     return res
+// }
+// console.log(arr("hello",'blue',true,"blue"));
+
+
+// 9-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.push("all")
+//     return res
+// }
+//  console.log(arr("world",'hello',false));
+
+
+// 10-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.pop()
+//     return res
+// }
+//  console.log(arr("world",'hello',false));
+
+// 11-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.shift()
+//     return res
+// }
+//  console.log(arr("lion",'rabbit',5));
+
+
+// 12-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     res.unshift("tiger")
+//     return res
+// }
+//  console.log(arr("lion",'rabbit',5));
+
+
+// 13-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     return res.includes("rabbit")
+// }
+//  console.log(arr("lion",'rabbit',5));
+
+
+
+// 14-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     return res.indexOf("rabbit")
+// }
+//  console.log(arr("lion",'rabbit',5));
+
+
+// 15-masala
+// function arr(arg1,arg2,arg3,arg4) {
+//     let res =[arg1,arg2,arg3,arg4];
+//     return res.lastIndexOf("cat")
+// }
+//  console.log(arr("cat",'dog',5,"cat"));
+
+
+
+// 16-masala
+function arr(arg1,arg2,arg3,arg4) {
+    let res =[arg1,arg2,arg3,arg4];
+    res.splice(2,2)
+    return res
+}
+ console.log(arr("cat",'dog',5,"cat"));
+
+
+
+
+
+
+
