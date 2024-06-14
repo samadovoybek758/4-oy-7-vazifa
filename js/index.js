9fb30ac7f281271dec37a56ea07d20974ccd2d9e
samadovoybek758 <oybek22samadov@gmail.com>
2024-06-14 15:15:17 +0500

    secondly

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 // 1-masala
 // function colors(color1,color2,color3) {
 //     let res =[color1,color2,color3];
@@ -18,10 +19,9 @@
 // 3-masala    !!!
 // function colors(color1,color2,color3) {
 //     let res =[color1,color2,color3];
-//     return res[0],res[res.length-1];
-    
+//     return res[0], res[1];
 // }
-// console.log(colors("red",'blue','green'));
+// console.log(colors("blue","red",'green'));
 
 
 
@@ -161,6 +161,164 @@
 
 
 
+// 21-masala
+// function arr(arg1,arg2,arg3) {
+
+//     let res =[arg1,arg2,arg3];
+//     let eng_katta=0;
+//     for (let i = 0; i <= arr.length; i++) {
+//         if (res[i]>eng_katta) {
+//             eng_katta=res[i] 
+//         }   
+//     }
+//     return eng_katta
+// }
+//  console.log(arr(23,3,8,))
+
+
+
+// 22-masala
+// function arr(arg1,arg2,arg3) {
+
+//     let res =[arg1,arg2,arg3];
+//     let eng_kichig=0;
+//     for (let i = 0; i <= arr.length; i++) {
+//         if (res[i]>eng_kichig) {
+//             eng_kichig=res[i] 
+//         }   
+//     }
+//     return eng_kichig
+// }
+//  console.log(arr(6,3,8,))
+
+
+
+
+// 23-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     let sum=0;
+//     for (let i = 0; i < res.length; i++) {
+//         sum=res[i] +sum;
+//     }
+//     return sum/res.length;
+// };
+//  console.log(arr(6,4,8,));
+
+
+
+
+// 24-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     let res2 =[]
+//     for (let i = 0; i < arr.length; i++) {
+//        res2.unshift(res[i])
+//     }
+//     return res2
+// }
+//  console.log(arr(6,3,8,))
+
+
+
+// 25-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     let res2=[]
+//     for (let i = 0; i < arr.length; i++) {
+//         if (res[i]>0) {
+//             res2.unshift(res[i])
+//         }
+//     }
+//     return res2
+// }
+//  console.log(arr(6,-4,8,))
+
+
+
+// 26-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     let res2 =[]
+//     for (let i = 0; i < arr.length; i++) {
+//        res2.push(res[i]*2)
+//     }
+//     return res2
+// }
+//  console.log(arr(6,3,8,))
+
+
+
+
+// 27-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     let res2 =[]
+//     for (let i = 0; i < arr.length; i++) {
+//        res2.push(res[i]**2)
+//     }
+//     return res2
+// }
+//  console.log(arr(6,3,8,))
+
+
+
+
+// 28-masala!!!!!
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     return res.pop()
+// }
+// let a = +prompt("elementni kiriting");
+// console.log(arr(6,3,8,))
+
+
+
+
+
+
+// 29-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     return res.includes(a)
+// };
+// let a = +prompt("elementni kiriting");
+//  console.log(arr(6,3,8,));
+
+
+
+
+// 30-masala
+
+
+// 31-masala
+// function arr(arg1,arg2,arg3) {
+//     let res =[arg1,arg2,arg3];
+//     let sum=0;
+//     for (let i = 0; i < res.length; i++) {
+//         sum=res[i] +sum;
+//     }
+//     return sum;
+// };
+//  console.log(arr(6,4,8,));
+
+
+
+
+// 32-masala!!!
+
+
+// 33-masala
+function arr(arg1,arg2,arg3,arg4,arg5,arg6) {
+    let res =[arg1,arg2,arg3];
+    let res2=[arg4,arg5,arg6]
+    for (let i = 0; i < res2.length; i++) {
+       res.push(res2[i])
+    }
+    return res
+};
+ console.log(arr(6,4,8,5,56,34));
+
 
 
 // massiv metodlariga oid masalalar
@@ -297,15 +455,12 @@
 
 
 // 16-masala
-function arr(arg1,arg2,arg3,arg4) {
-    let res =[arg1,arg2,arg3,arg4];
-    res.splice(2,2)
-    return res
-}
- console.log(arr("cat",'dog',5,"cat"));
-
-
-
+// function arr(arg1,arg2,arg3,arg4) {
+//     let res =[arg1,arg2,arg3,arg4];
+//     res.splice(2,2)
+//     return res
+// }
+//  console.log(arr("cat",'dog',5,"cat"));
 
 
 
